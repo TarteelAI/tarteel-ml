21d8e54772ca54468f182c28c4cb743aec21a751
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2019-08-03 18:28:37 -0400

    Update download.py to only download and process to a standard format evaluated pieces of audio.

--- download.py
+++ download.py
@@ -5,16 +5,18 @@ A file for downloading audio recordings from tarteel.io.
 Originally created by kareemn on Jan. 1, 2019.
 https://github.com/kareemn/Tarteel-ML/blob/master/download.py
 
-Modified by Hamzah Khan (khanh111) and added to the tarteel.io/Tarteel-ML on Jan. 12.
+Modified by Hamzah Khan (khanh111) and added to the tarteel.io/Tarteel-ML on Jan. 12, 2019.
+
+Refactored completely by Hamzah Khan (khanh111) on Aug. 03, 2019.
 """
 
+import utils.audio as audio_utils
 import utils.files as file_utils
 import utils.recording as recording_utils
 import csv
 import os
 import requests
 import wave
-import webrtcvad
 
 from urllib.parse import urlparse
 from argparse import ArgumentParser
@@ -22,20 +24,22 @@ from argparse import ArgumentParser
 # Define argument constants.
 TARTEEL_V1_CSV_URL = "https://d2sf46268wowyo.cloudfront.net/datasets/tarteel_v1.0.csv"
 TARTEEL_LIVE_CSV_URL = "https://www.tarteel.io/download-full-dataset-csv"
+TARTEEL_V1_LABELED_CSV_URL = "https://tarteel-static.s3-us-west-2.amazonaws.com/labels/tarteel_v1.0_labeled.csv"
 
 
 parser = ArgumentParser(description='Tarteel Audio Downloader')
-parser.add_argument('--csv_url', type=str, default=TARTEEL_V1_CSV_URL)
+parser.add_argument('--csv_url', type=str, default=TARTEEL_V1_LABELED_CSV_URL)
 parser.add_argument('--local_csv_filename', type=str, default='local.csv')
 parser.add_argument('--cache_dir', type=str, default='.cache')
-parser.add_argument('--vad_check', type=bool, default=True)
 parser.add_argument('-u', '--use_cache', type=bool, default=False)
 parser.add_argument('-v', '--verbose', type=bool, default=False)
 parser.add_argument('-s', '--surah', type=int)
+parser.add_argument('-k', '--keep_downloaded_audio', type=bool, default=False)
 args = parser.parse_args()
 
 DATASET_CSV_CACHE = 'csv'
-AUDIO_CACHE = 'audio'
+DOWNLOADED_AUDIO_CACHE = 'downloaded_audio'
+RAW_AUDIO_CACHE = 'raw_audio'
 
 # Define constants.
 NO_FRAMES_VALUE = 1.0
@@ -46,6 +50,19 @@ WEBRTCVAD_SUPPORTED_SAMPLE_RATES_HZ = [8000, 16000, 32000, 48000]
 
 def does_cached_csv_dataset_exist(path_to_dataset_csv):
     return os.path.isfile(path_to_dataset_csv)
+
+def convert_to_bool(str):
+    """
+    Converts string versions of true or false to bools.
+    """
+    upper_string = str.upper()
+    if upper_string == "TRUE":
+        return True
+    elif upper_string == "FALSE":
+        return False
+    else:
+        raise TypeError("Provided string is neither 'true' nor 'false'.")
+
 
 def download_csv_dataset(csv_url, dataset_csv_path, verbose=False):
     if verbose:
@@ -60,65 +77,36 @@ def download_csv_dataset(csv_url, dataset_csv_path, verbose=False):
 
 def get_dataset_entries(path_to_dataset_csv):
     """
-    A function to parse and return the rows of the dataset csv.
+    A function to parse and return the header and rows of the dataset csv.
     """
-    file = open(path_to_dataset_csv, "r")
-    content = file.read()
-    csv_data = csv.reader(content.splitlines(), delimiter=',')
-    entries = list(csv_data)
-    return entries
+    with open(path_to_dataset_csv, 'r') as file:
+        content = file.read()
+        csv_data = csv.reader(content.splitlines(), delimiter=',')
+        entries = list(csv_data)
+        header_row = entries[0]
+        dataset = entries[1:]
+        return header_row, dataset
 
-def download_audio(row, local_download_dir, verbose=False):
+def download_entry_audio(entry, raw_audio_dir, audio_dir, use_cache=True, verbose=False):
     """
     Downloads an audio recording given its entry in a Tarteel dataset csv.
     """
-    surah_num = int(row[0])
-    ayah_num = int(row[1])
-    url = row[2]
-    parsed_url = urlparse(url)
-    wav_filename = os.path.basename(parsed_url.path)
-    local_download_path = recording_utils.get_path_to_recording(local_download_dir, surah_num, ayah_num, wav_filename)
+    surah_num, ayah_num, url, _, _, _, _, _, _, _, _ = entry
 
-    # Download and save the audio recording to the given path.
-    try:
-        with requests.Session() as s:
-            if verbose:
-                print("Downloading", url, "to", local_download_path, ".")
-            download = s.get(url)
-            dirname = os.path.dirname(local_download_path)
-            if not os.path.exists(dirname):
-                os.makedirs(dirname)
-            file = open(local_download_path, "wb")
-            file.write(download.content)
-            file.close()
+    # Convert data to correct type from string.
+    surah_num = int(surah_num)
+    ayah_num = int(ayah_num)
 
-    except:
-        # If the download fails, print an error and exit the function.
-        print("Audio file", local_download_path, "could not be opened.")
-        return
+    # Ensure that the directories that the recording will be saved to exist.
+    downloaded_ayah_audio_dir = file_utils.prepare_ayah_directory(raw_audio_dir, surah_num, ayah_num)
+    ayah_audio_dir = file_utils.prepare_ayah_directory(raw_audio_dir, surah_num, ayah_num)
 
-    # Check if the wave header is valid and if so, get the desired info. This deletes files with invalid headers.
-    has_valid_header, wav_bytes, sample_rate_hz, num_channels = recording_utils.open_recording(local_download_path)
+    # Download and save the raw audio recording to the given path.
+    downloaded_audio_path = recording_utils.download_recording_from_url(url, downloaded_ayah_audio_dir, use_cache=use_cache, verbose=verbose)
 
-    if has_valid_header:
-        # Note: webrtcVAD does not currently support 44.1MHz, so we have no way of checking those files for empty audio.
-        recording_has_speech = recording_utils.has_speech(
-            wav_bytes, 
-            sample_rate_hz, 
-            num_channels, 
-            non_speech_threshold_fraction=DEFAULT_NON_SPEECH_THRESHOLD_FRACTION, 
-            verbose=args.verbose)
-
-        if args.vad_check and sample_rate_hz in WEBRTCVAD_SUPPORTED_SAMPLE_RATES_HZ and not recording_has_speech:
-            print("Audio file", local_download_path, "does not have speech according to VAD. Removing.")
-            os.remove(local_download_path)
-
-    if sample_rate_hz not in WEBRTCVAD_SUPPORTED_SAMPLE_RATES_HZ+[44100] and sample_rate_hz != None:
-        # If unsupported sample frequency, remove the file.
-        print("File ", local_download_path, " has an unsupported sample frequency %d. Removing." % sample_rate_hz)
-        if os.path.exists(local_download_path):
-            os.remove(local_download_path)
-
+    # Convert raw audio to a standard format.
+    # This audio is being converted to 44.1 MHz, 16bps audio (Google Speech Recognition's requirements).
+    raw_audio_path = audio_utils.convert_audio(downloaded_audio_path, raw_audio_dir, use_cache=use_cache, verbose=verbose)
 
 if __name__ == "__main__":
 
@@ -129,13 +117,15 @@ if __name__ == "__main__":
     use_cache = args.use_cache
     verbose = args.verbose
     surah_to_download = args.surah
+    keep_downloaded_audio = args.keep_downloaded_audio
 
     # Prepare all requisite cache directories.
-    subcache_directory_names = (DATASET_CSV_CACHE, AUDIO_CACHE)
-    csv_cache_dir, audio_cache_dir = file_utils.prepare_cache_directories(subcache_directory_names,
-                                                                          cache_directory=cache_directory,
-                                                                          use_cache=use_cache,
-                                                                          verbose=verbose)
+    subcache_directory_names = (DATASET_CSV_CACHE, DOWNLOADED_AUDIO_CACHE, RAW_AUDIO_CACHE)
+    csv_cache_dir, downloaded_audio_dir, raw_audio_dir = file_utils.prepare_cache_directories(
+                                                  subcache_directory_names,
+                                                  cache_directory=cache_directory,
+                                                  use_cache=use_cache,
+                                                  verbose=verbose)
 
     # Create path to dataset csv.
     path_to_dataset_csv = os.path.join(csv_cache_dir, dataset_csv_filename)
@@ -154,9 +144,16 @@ if __name__ == "__main__":
             print("Using cached copy of dataset csv at {}.".format(path_to_dataset_csv))
 
     # Read the rows of dataset csv.
-    entries = get_dataset_entries(path_to_dataset_csv)
+    header_row, entries = get_dataset_entries(path_to_dataset_csv)
+
+    # Filter out recordings that have been evaluated and labeled falsely.
+    labeled_entries = [entry for entry in entries if convert_to_bool(entry[9]) and convert_to_bool(entry[10])]
 
     # Download the audio in the dataset.
-    for entry in entries:
+    for entry in labeled_entries:
         if entry[0] == str(surah_to_download):
-            sample_rate_hz = download_audio(entry, audio_cache_dir, verbose=verbose)
+            sample_rate_hz = download_entry_audio(entry, downloaded_audio_dir, raw_audio_dir, use_cache=use_cache, verbose=verbose)
+
+    # If we don't want to keep the raw audio, remove it from the cache.
+    if not keep_downloaded_audio:
+        file_utils.delete_cache_directories(cache_directory=downloaded_audio_dir)
