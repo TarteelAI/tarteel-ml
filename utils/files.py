1012fa838bbfcc6f8f84d00762ccbf5d88182e3f
Anas Abou Allaban <aabouallaban@pm.me>
2019-12-23 03:52:09 -0600

    Update utils with better docstrings and types. Add features
    Signed-off-by: Anas Abou Allaban <aabouallaban@pm.me>

--- utils/files.py
+++ utils/files.py
@@ -1,30 +1,35 @@
-"""
-Utility functions for directory and file IO.
-Author: Fahim Dalvi, Hamzah Khan
-Date: May 3, 2019
+"""Utility functions for directory and file IO.
 
-A file for helper functions that read and manipulate non-audio files and directories. Helpers in this file should NOT
-require an internet connection to use.
+A file for helper functions that read and manipulate non-audio files and directories.
+Helpers in this file should NOT require an internet connection to use.
 """
 
 import csv
+import json
+import logging
 import os
+from typing import List
+from typing import Tuple
+from typing import Union
 import random
 import shutil
 import string
 
 DEFAULT_CACHE_DIRECTORY = '.cache'
 
-def does_cached_csv_dataset_exist(path_to_dataset_csv):
+
+def does_cached_csv_dataset_exist(path_to_dataset_csv: str) -> bool:
     return os.path.isfile(path_to_dataset_csv)
 
-def get_path_to_dataset_csv(csv_cache_dir, dataset_csv_filename):
+
+def get_path_to_dataset_csv(
+        csv_cache_dir: str,
+        dataset_csv_filename: str) -> str:
     return os.path.join(csv_cache_dir, dataset_csv_filename)
 
-def get_dataset_entries(path_to_dataset_csv):
-    """
-    A function to parse and return the header and rows of the dataset csv.
-    """
+
+def get_dataset_entries(path_to_dataset_csv: str) -> Tuple[List, List]:
+    """Parse and return the header and rows of the dataset csv."""
     with open(path_to_dataset_csv, 'r') as file:
         content = file.read()
         csv_data = csv.reader(content.splitlines(), delimiter=',')
@@ -33,10 +38,13 @@ def get_dataset_entries(path_to_dataset_csv):
         dataset = entries[1:]
         return header_row, dataset
 
-def prepare_cache_directories(subdirectory_names_tuple, cache_directory=DEFAULT_CACHE_DIRECTORY, use_cache=True, verbose=False):
-    """
-    Creates a set of directories to be used for caching different pieces of data.
-    """
+
+def prepare_cache_directories(
+        subdirectory_names_tuple: Tuple,
+        cache_directory: str = DEFAULT_CACHE_DIRECTORY,
+        use_cache: bool = True,
+        verbose: bool = False) -> Tuple:
+    """Create a set of directories to be used for caching different pieces of data."""
     # If we don't use the cache, create a common temporary cache directory.
     if not use_cache:
         cache_directory = create_temporary_cache_directory_name()
@@ -45,38 +53,43 @@ def prepare_cache_directories(subdirectory_names_tuple, cache_directory=DEFAULT_
     subcache_directory_list = []
 
     for subdirectory_name in subdirectory_names_tuple:
-        subcache_directory_path = create_cache_directory(subdirectory_name, cache_directory=cache_directory, use_cache=True, verbose=verbose)
+        subcache_directory_path = create_cache_directory(
+            subdirectory_name, cache_directory=cache_directory, use_cache=True, verbose=verbose)
         subcache_directory_list.append(subcache_directory_path)
 
     return tuple(subcache_directory_list)
 
-def prepare_ayah_directory(directory_path, surah_num, ayah_num):
-    """
-    Prepares a directory structure for an ayah. Returns the directory path.
+
+def prepare_ayah_directory(directory_path, surah_num, ayah_num) -> str:
+    """Prepare a directory structure for an ayah.
+
+    Returns:
+        ayah_directory_path The path to the directory.
     """
     ayah_directory_path = os.path.join(directory_path, 's{}'.format(surah_num), 'a{}'.format(ayah_num))
     os.makedirs(ayah_directory_path, exist_ok=True)
     return ayah_directory_path
 
-def create_temporary_cache_directory_name():
-    """
-    Create a temporary cache directory anme.
-    """
+
+def create_temporary_cache_directory_name() -> str:
+    """Create a temporary cache directory name."""
     temp_directory_name = ''.join([random.choice(string.ascii_letters) for i in range(10)])
     return ".tmp-" + temp_directory_name
 
-def create_cache_directory(directory_subpath, cache_directory=DEFAULT_CACHE_DIRECTORY, use_cache=True, verbose=False):
-    """
-    Creates the cache directory structure for the given directory.
-    """
+
+def create_cache_directory(
+        directory_subpath: str,
+        cache_directory: str = DEFAULT_CACHE_DIRECTORY,
+        use_cache: bool = True,
+        verbose: bool = False) -> str:
+    """Create the cache directory structure for the given directory."""
     # Ensure the cache_directory always exists.
     os.makedirs(cache_directory, exist_ok=True)
 
     # If we don't want to use the cache, create a temporary cache to place files in.
     if not use_cache:
         cache_directory = create_temporary_cache_directory_name()
-        if verbose:
-            print("Created temporary empty directory at ", cache_directory, ".")
+        logging.info("Created temporary empty directory at {}.".format(cache_directory))
 
     # Create cache path.
     desired_cache_dir = os.path.join(cache_directory, directory_subpath)
@@ -84,22 +97,47 @@ def create_cache_directory(directory_subpath, cache_directory=DEFAULT_CACHE_DIRE
 
     return desired_cache_dir
 
-def clean_cache_directories(cache_directory=DEFAULT_CACHE_DIRECTORY, verbose=False):
-    """
-    Removes all cached files (but keeps the cache structure intact for future
-    use).
-    """
+
+def clean_cache_directories(cache_directory: str = DEFAULT_CACHE_DIRECTORY) -> None:
+    """Remove all cached files (but keeps the cache structure intact for future use)."""
     # If the cache directory doesn't exist, then just make an empty one.
     if not os.path.isdir(cache_directory):
         os.makedirs(cache_directory)
         
     for subdirectory in os.listdir(cache_directory):
-        if verbose:
-            print("Removing cache_subdirectory %s." % (subdirectory))
+        logging.info("Removing cache_subdirectory {}.".format(subdirectory))
         shutil.rmtree(os.path.join(cache_directory, subdirectory))
 
-def delete_cache_directories(cache_directory=DEFAULT_CACHE_DIRECTORY):
-    """
-    Deletes the entire cache (files and structure).
-    """
+
+def delete_cache_directories(cache_directory: str = DEFAULT_CACHE_DIRECTORY) -> None:
+    """Delete the entire cache (files and structure)."""
     shutil.rmtree(cache_directory)
+
+
+def get_all_files_in_directory(directory: str) -> List:
+    (_, _, filenames) = next(os.walk(directory))
+    return filenames
+
+
+def get_file_size(path: str) -> Union[int, None]:
+    try:
+        return os.path.getsize(path)
+    except os.error as e:
+        logging.warning(
+            "Unable to get the file size for {}. Got the following error: {}".format(path, e))
+        return None
+
+
+def change_file_extension(filename: str, extension) -> str:
+    audio_filename = os.path.basename(filename)
+    return "{}.{}".format(audio_filename[:-4], extension)
+
+
+def open_json(json_file: str) -> dict:
+    with open(json_file) as file:
+        return json.load(file)
+
+
+def write_json(json_file: str, json_dict: dict) -> None:
+    with open(json_file, 'w') as file:
+        json.dump(json_dict, file)
--- utils/quran_helper.py
+++ utils/quran_helper.py
@@ -1,9 +1,9 @@
+import logging
 import os
 from os import path
 from typing import Union
 
-from util import file_handler
-from util.logging import print_warn
+from utils import files
 
 QURAN_FILE = path.join(path.dirname(os.getcwd()), 'data/quran.json')
 MAX_SURAH_NUMBER = 114
@@ -11,14 +11,15 @@ MIN_SURAH_NUMBER = 1
 
 
 class Quran:
-	"""An object used to get a Surah's Ayahs along with transcript loaded from JSON.
+    """An object used to get a Surah's Ayahs along with transcript loaded from JSON.
 
-	The JSON file is ~12MB when loaded in memory which is acceptable. A better alternative
-	is to use a database, however that is too much overhead for the simple operations we want to
-	perform.
-	"""
+    The JSON file is ~2MB when loaded in memory which is acceptable. A better alternative
+    is to use a database, however that is too much overhead for the simple operations we want to
+    perform.
+    """
+
     def __init__(self, quran_file: str = QURAN_FILE):
-        self._quran_json = file_handler.open_json(quran_file)
+        self._quran_json = files.open_json(quran_file)
         self._quran_json = self._quran_json['quran']['surahs']
 
     def get_quran(self):
@@ -26,13 +27,13 @@ class Quran:
 
     def surah(self, surah_number: int) -> Union[dict, None]:
         if (surah_number > MAX_SURAH_NUMBER) or (surah_number < MIN_SURAH_NUMBER):
-            print_warn('Surah number out of bounds')
+            logging.warning('Surah number out of bounds')
             return None
         try:
             surah = self._quran_json[surah_number-1]
             return surah
         except KeyError:
-            print_warn('Surah number not found')
+            logging.warning('Surah number not found')
             return None
 
     def ayah(self, surah_number: int, ayah_number: int) -> Union[dict, None]:
@@ -40,7 +41,7 @@ class Quran:
             ayah = self.surah(surah_number).get('ayahs')[ayah_number-1]
             return ayah
         except KeyError:
-            print_warn('Ayah not found')
+            logging.warning('Ayah not found')
             return None
 
     def get_ayah_text(self, surah_number: int, ayah_number: int) -> Union[str, None]:
