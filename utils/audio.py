fa47a44d1f2c458dfc845669868b603b1133f952
Anas Abou Allaban <aabouallaban@pm.me>
2019-12-24 21:05:32 -0600

    Make processing multithreaded, add audio info generator, refactor audio utils
    Signed-off-by: Anas Abou Allaban <aabouallaban@pm.me>

--- utils/audio.py
+++ utils/audio.py
@@ -1,71 +1,19 @@
 """Utility functions for processing audio."""
-__author__ = 'Hamzah Khan'
 
 import logging
 import os
-import re
 import subprocess
 from typing import Optional
 from typing import Tuple
-import wave
 
 import librosa
+from pydub import AudioSegment
+from pydub.playback import play
+from pydub.utils import mediainfo_json
 import soundfile as sf
 import tensorflow as tf
 
-
-DEFAULT_AUDIO_FORMAT = 'raw'
-DEFAULT_SAMPLE_RATE = 44100  # 44100 for Google STT, 16000 for DeepSpeech
-
-
-def detect_audio_type(audio_filepath: str) -> str:
-    """Returns the type of audio encoding. 
-    
-    Currently uses ffprobe to get this information.
-
-    Args:
-        audio_filepath: Absolute path to the audio file to process.
-
-    Returns:
-        audio_type: The audio encoding type.
-    """
-    command_args = ['ffprobe', '-hide_banner', audio_filepath]
-    ffprobe_output = subprocess.check_output(command_args, stderr=subprocess.STDOUT, universal_newlines=True)
-    m = re.search(r'Audio:\s(\w+)\s', ffprobe_output)
-
-    if m:
-        audio_type = m.group(1)
-    else:
-        audio_type = 'unknown'
-
-    return audio_type
-
-
-def convert_to_wav(
-        input_file: str,
-        output_file: str,
-        sample_rate: int = 16000) -> bool:
-    """Convert a file to wav format with librosa.
-
-    Deletes empty audio files.
-
-    Args:
-        input_file: File to convert
-        output_file: File to write to
-        sample_rate: Desired audio sample rate in Hz (independent of original format)
-
-    Returns:
-        result: True if wrote successfully. False otherwise
-    """
-    try:
-        x, sr = librosa.load(input_file, sr=sample_rate)
-        sf.write(output_file, x, sr)
-        return True
-    except ValueError as e:
-        if 'Input signal length=0' in str(e):
-            logging.info("Found empty audio file. Removing...")
-            os.remove(input_file)
-            return False
+DEFAULT_SAMPLE_RATE = 16000  # 44100 for Google STT, 16000 for DeepSpeech
 
 
 def convert_to_raw(input_file: str, bitrate: int, sample_rate: int, output_file: str) -> str:
@@ -81,46 +29,80 @@ def convert_to_raw(input_file: str, bitrate: int, sample_rate: int, output_file:
     return subprocess.check_output(command_args, stderr=subprocess.STDOUT, universal_newlines=True)
 
 
-def open_wave_file_tf(path_to_audio: str) -> Tuple[tf.Tensor, tf.Tensor]:
-    """Opens a wave file using Tensorflow.
+class AudioFile:
+    def __init__(self, filepath: str):
+        self._filepath = filepath
+        self.duration = None
+        self.size = None
+        self.format_name = None
+        self.sample_rate = None
+        self.channels = None
+        self.bits = None
+        self.codec = None
+        self.data = None
+        self._deleted = False
+        self._get_info()
 
-    Used to check if it's possible to read a wave file the the TF library.
-    Throws on any errors.
-    """
-    audio_binary = tf.io.read_file(path_to_audio)
-    return tf.audio.decode_wav(audio_binary, desired_channels=1)
+    def _get_info(self):
+        info = mediainfo_json(self._filepath)
+        stream = info.get('streams')[0]
+        media_format = info.get('format')
+        self.duration = media_format.get('duration')
+        self.size = media_format.get('size')
+        self.format_name = media_format.get('format_name')
+        self.sample_rate = stream.get('sample_rate')
+        self.channels = stream.get('channels')
+        self.bits = stream.get('bits_per_sample')
+        self.bit_rate = media_format.get('bit_rate')
+        self.code = stream.get('codec_name')
+        try:
+            self.data, _ = librosa.load(self._filepath)
+        except ValueError as e:
+            if 'Input signal length=0' in str(e):
+                logging.info("Found empty audio file.")
+                self.data = None
 
+    def is_wav(self):
+        return self.format_name == 'wav'
 
-def open_wave_file(path_to_audio: str) -> Tuple[bool, Optional[bytes], Optional[int], Optional[int]]:
-    """Check if the audio file has a proper wave header.
+    def is_empty(self):
+        return (self.duration is None) or (self.size == 0)
 
-    Proper is defined as whether the Python `wave` module can open the file.
+    def is_deleted(self):
+        return self._deleted
 
-    Returns:
-        (False, None, None, None) if the header is invalid.
-        (True, wav_bytes, sample_rate_hz, num_channels) if header is valid.
-    """
-    # TODO(piraka9011) Refactor to use wf.getparams() instead and return two objects instead.
-    try:
-        with wave.open(path_to_audio, 'rb') as wf:
-            wav_bytes = wf.readframes(wf.getnframes())
-            sample_rate_hz = wf.getframerate()
-            num_channels = wf.getnchannels()
-            return True, wav_bytes, sample_rate_hz, num_channels
-    except wave.Error as e:
-        logging.warning(
-            "File with invalid wave header found:\n{}\nException: {}".format(path_to_audio, e))
-        return False, None, None, None
+    def resample(self, sample_rate: int = DEFAULT_SAMPLE_RATE, inplace: bool = False):
+        self.data, self.sample_rate = librosa.load(self._filepath, sample_rate)
+        if not inplace:
+            sf.write(self._filepath, self.data, sample_rate)
 
+    def play(self):
+        to_play = AudioSegment.from_file(self._filepath)
+        play(to_play)
 
-def save_wave_file(
-        filename: str,
-        audio_data: bytes,
-        sample_rate_hz: int,
-        num_channels: int = 1) -> None:
-    """Write a WAVE file to disk."""
-    with wave.open(filename, 'wb') as file:
-        file.setnchannels(num_channels)
-        file.setframerate(sample_rate_hz)
-        file.setsampwidth(2)
-        file.writeframes(audio_data)
+    def save_as(self,
+                output_file: Optional[str],
+                sample_rate: Optional[int],
+                output_format: Optional[str],
+                subtype: str = 'PCM_16') -> None:
+        output_path = output_file if output_file else self._filepath
+        sr = sample_rate if sample_rate else self.sample_rate
+        fmt = output_format if output_format else self.format_name
+        sf.write(output_path, self.data, sr, subtype, format=fmt)
+
+    def delete(self):
+        os.remove(self._filepath)
+        self._deleted = True
+
+    def load_with_tf(self) -> Optional[Tuple[tf.Tensor, tf.Tensor]]:
+        """Opens a wave file using Tensorflow.
+
+        Used to check if it's possible to read a wave file the the TF library.
+        Throws on any errors.
+        """
+        if self._deleted:
+            logging.warning("File is deleted.")
+            return
+
+        audio_binary = tf.io.read_file(self._filepath)
+        return tf.audio.decode_wav(audio_binary, desired_channels=self.channels)
